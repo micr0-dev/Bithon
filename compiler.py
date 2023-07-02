b62f93ea615b61ae0bb6ec5c95bbec570ea0e998
Micr0Arch <miraslauk@gmail.com>
2023-07-01 20:43:24 -0400

    Working lexer

---
+++ compiler.py
@@ -0,0 +1,140 @@
+import ply.yacc as yacc
+from lexer import tokens
+from lexer import lexer
+
+tokens = tokens
+
+def p_program(p):
+    '''program : statements'''
+    pass
+
+def p_block(p):
+    '''block : INDENT statements DEDENT'''
+    pass
+
+def p_statements(p):
+    '''statements : statement
+                  | statements statement'''
+    pass
+
+
+def p_statement(p):
+    '''statement : if_statement
+                 | def_statement
+                 | loop_statement
+                 | return_statement
+                 | expression_statement
+                 | function_call
+                 | NEWLINE'''
+    pass
+
+def p_function_call(p):
+    '''function_call : IDENT arg'''
+    pass
+
+def p_if_statement(p):
+    '''if_statement : IFF expression block
+                    | IFF expression block else_statement'''
+    pass
+
+def p_else_statement(p):
+    '''else_statement : ELS block'''
+    pass
+
+def p_def_statement(p):
+    '''def_statement : DEF IDENT arg block'''
+    pass
+
+def p_arg(p):
+    '''arg : IDENT
+           | IDENT arg 
+           | '''
+    pass
+
+def p_loop_statement(p):
+    '''loop_statement : LOP IDENT expression expression block'''
+    pass
+
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = float(p[1])
+
+def p_expression_string(p):
+    'expression : STRING'
+    p[0] = p[1][1:-1]  # remove the quotation marks
+
+def p_expression_bool(p):
+    'expression : BOOL'
+    p[0] = bool(p[1])
+
+def p_expression_not(p):
+    'expression : NOT expression'
+    p[0] = not p[2]
+
+def p_expression_ident(p):
+    'expression : IDENT'
+    p[0] = p[1]
+
+def p_equal(p):
+    'expression : expression EQUAL expression'
+    p[0] = p[1] == p[3]
+
+def p_expression_plus(p):
+    'expression : expression PLUS expression'
+    p[0] = p[1] + p[3]
+
+def p_expression_minus(p):
+    'expression : expression MINUS expression'
+    p[0] = p[1] - p[3]
+
+def p_expression_mul(p):
+    'expression : expression MUL expression'
+    p[0] = p[1] * p[3]
+
+def p_expression_div(p):
+    'expression : expression DIV expression'
+    p[0] = p[1] / p[3]
+
+def p_expression_modulus(p):
+    'expression : expression MODULUS expression'
+    p[0] = p[1] % p[3]
+
+def p_expression_group(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+def p_return_statement(p):
+    '''return_statement : RET expression'''
+    pass
+
+def p_expression_statement(p):
+    'expression_statement : expression NEWLINE'
+    p[0] = p[1]
+
+def p_error(p):
+    if p:
+        print("Syntax error in input! At line: ", p.lineno, "\n", p)
+    else:
+        print("Syntax error at EOF")
+
+
+parser = yacc.yacc()
+
+
+# open test.bthn file and get contents to set as program
+with open('test.bthn', 'r') as f:
+    program = f.read()
+
+print( "Program: \n", program, "\n")
+
+print(parser.parse(program, lexer=lexer))
+
+# print("\n")
+
+# lexer.input(program)
+
+# while True:
+#     tok = lexer.token()
+#     if not tok:
+#         break
+#     print(tok)
---
+++ lexer.py
@@ -0,0 +1,83 @@
+import ply.lex as lex
+
+reserved = {
+    'iff' : 'IFF',
+    'els' : 'ELS',
+    'def' : 'DEF',
+    'lop' : 'LOP',
+    'ret' : 'RET',
+    'true' : 'BOOL',
+    'false' : 'BOOL',
+    'not' : 'NOT',
+    'mul' : 'MUL',
+    'add' : 'PLUS',
+    'sub' : 'MINUS',
+    'div' : 'DIV',
+    'mod' : 'MODULUS',
+    'eql' : 'EQUAL',
+}
+
+tokens = [
+    
+     'NUMBER', 'STRING',  
+     'NEWLINE', 'INDENT', 'DEDENT', 
+     'LPAREN', 'RPAREN', 
+     'IDENT'
+]+ list(set(reserved.values()))
+
+t_NUMBER = r'\b\d+(\.\d+)?\b'
+t_STRING = r'"[^"\\]*(?:\\.[^"\\]*)*"'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MUL = r'\*'
+t_DIV = r'/'
+t_MODULUS = r'%'
+t_EQUAL = r'='
+
+t_ignore = ' '
+
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    t.lexer.indents_count = 0
+    while True:  # Loop to count indents or dedents
+        try:
+            if lexer.lexdata[lexer.lexpos] in ' \t':
+                t.lexer.indents_count += 1
+                lexer.lexpos += 1
+            else:
+                break
+        except IndexError:
+            break
+    if t.lexer.indents_count > t.lexer.indents[-1]:  # It is an indent
+        t.type = 'INDENT'
+        t.lexer.indents.append(t.lexer.indents_count)
+    elif t.lexer.indents_count < t.lexer.indents[-1]:  # It is a dedent
+        t.type = 'DEDENT'
+        t.lexer.indents.pop()
+    return t
+
+def t_IDENT(t):
+    r'\b[a-zA-Z_][a-zA-Z0-9_]*\b'
+    t.type = reserved.get(t.value,'IDENT')    # Check for reserved words
+    return t
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+def t_eof(t):
+    if len(lexer.indents) > 1:
+        lexer.indents.pop()
+        t.type = 'DEDENT'
+        return t
+    else:
+        return None
+
+
+lexer = lex.lex()
+lexer.indents = [0]
+lexer.indents_count = 0
